a8656eb842108136bc2ba501df9efd743227c357
irafajs <irafajs@gmail.com>
2024-03-14 20:15:07 +0200

    TASK09 UPDATE1

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -9,6 +9,7 @@ from typing import Sequence, Tuple, List, Iterable
 
 ret_lst = Iterable[Sequence]
 
+
 def element_length(lst: ret_lst) -> List[Tuple[Sequence, int]]:
     """accept sequence args and return List, Tupe"""
     return [(item, len(item)) for item in lst]
