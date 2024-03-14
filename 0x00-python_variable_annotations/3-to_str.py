cbcac53f7e053d9cbd3e612d8103be14a0ca2295
irafajs <irafajs@gmail.com>
2024-03-14 15:49:09 +0200

    TASK03

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Shebang to create a PY script
+"""
+
+
+def to_str(n: float) -> str:
+    """method that takes a float argument and return a string"""
+    return str(n)
