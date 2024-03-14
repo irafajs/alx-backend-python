b87a1d7dc1e0d1c23fe96a29f43ca5b8c20d2118
irafajs <irafajs@gmail.com>
2024-03-14 15:34:37 +0200

    TASK00

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Shebang to create a py code
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """method to handle input as string and return one"""
+    return str1 + str2
