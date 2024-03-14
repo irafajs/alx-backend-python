2970985b394a08fd0002f7ff5a56df0573a74c34
irafajs <irafajs@gmail.com>
2024-03-14 15:43:43 +0200

    TASK02

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Shebang to create a PY sript
+"""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """method that take arg as float and return a float"""
+    return math.floor(n)
