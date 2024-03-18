6e0e7324185b741be8dfc54831d12a84f01cd4a3
irafajs <irafajs@gmail.com>
2024-03-18 17:09:30 +0200

    TASK00

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Shebang to create a PY script
+"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Method to generate random number betweeb 0-10"""
+    rand_num = random.uniform(0, max_delay)
+    await asyncio.sleep(rand_num)
+    return rand_num
+
+
+async def main():
+    """main to run the whole code"""
+    max_delay = 10
+    result = await wait_random(max_delay)
+
+asyncio.run(main())
