e4f59c117c22f3b1ce2618fa6464272fbfe0f114
irafajs <irafajs@gmail.com>
2024-03-18 21:34:02 +0200

    TASK04

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Shebang to create a PY script
+"""
+
+
+import random
+import asyncio
+from typing import List
+
+basic = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """method to return a list of all delays"""
+    tasks = [basic(max_delay) for _ in range(n)]
+    completed_tasks, _ = await asyncio.wait(tasks)
+    delays = [task.result() for task in completed_tasks]
+    return sorted(delays)
