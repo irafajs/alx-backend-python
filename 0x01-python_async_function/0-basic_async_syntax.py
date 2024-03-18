7850a7a15a5036094db0c0bd5ce4edf10431246b
irafajs <irafajs@gmail.com>
2024-03-18 20:28:12 +0200

    ORIGINAL PUSH

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,7 +9,7 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """Method to generate random number betweeb 0-10"""
-    #rand_num = random.uniform(0, max_delay)
-    rand_num = random.random() * max_delay
+    rand_num = random.uniform(0, max_delay)
+    #rand_num = random.random() * max_delay
     await asyncio.sleep(rand_num)
     return rand_num
