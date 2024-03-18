ff620c5b9f3ba3ce38c2950464c7f7d7ac5f6edf
irafajs <irafajs@gmail.com>
2024-03-18 20:45:13 +0200

    TASK01

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,18 +8,12 @@ import random
 import asyncio
 from typing import List
 
-basic = __import__('0-basic_async_syntax').wait_random
+basic = __import__('0-basic_async_syntax')
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """method to return a list of all delays"""
-    tasks = [wait_random(max_delay) for _ in range(n)]
+    tasks = [basic.wait_random(max_delay) for _ in range(n)]
     completed_tasks, _ = await asyncio.wait(tasks)
     delays = [task.result() for task in completed_tasks]
     return delays
-
-async def main():
-    delay = await wait_n(n, max_delay)
-
-asyncio.run(main())
-
