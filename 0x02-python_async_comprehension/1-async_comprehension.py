2d9ee7148691e98f87552c4586d2340693a14fa4
irafajs <irafajs@gmail.com>
2024-03-19 14:58:54 +0200

    TASK01

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Shebang to create a PY script
+"""
+
+
+import asyncio
+import random
+from typing import Generator
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> Generator[float, None, None]:
+    """Collect 10 random number from async_gen and return 10 rundom num"""
+    async_gen = async_generator()
+    return [i async for i in async_gen]
