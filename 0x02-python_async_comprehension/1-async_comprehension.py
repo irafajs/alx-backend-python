766bc40199341d0a23f1f32a5fd394518879a20f
irafajs <irafajs@gmail.com>
2024-03-19 16:40:14 +0200

    TASK01 ONE MORE UPDATE

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -13,5 +13,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """Collect 10 random number from async_gen and return 10 rundom num"""
-    async_gen = async_generator()
-    return [i async for i in async_gen]
+    return [i async for i in async_generator()]
