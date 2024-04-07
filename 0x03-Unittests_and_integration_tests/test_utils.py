de6c1fb9fe6b00aadcbb53f8af6747cb7987115d
irafajs <irafajs@gmail.com>
2024-04-07 21:17:00 +0200

    TASK01

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -12,6 +12,7 @@ access_nested_map = __import__('utils').access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """class to test AccessNestedMap"""
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -19,4 +20,16 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        """method to test access_nested_map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), "Key 'a' not found in nested map"),
+        ({"a": 1}, ("a", "b"), "Key 'b' not found in nested map['a']"),
+        ])
+    def test_access_nested_map_exception(
+            self, nested_map, path, expected_message):
+        """method to test exception raised"""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), expected_message)
