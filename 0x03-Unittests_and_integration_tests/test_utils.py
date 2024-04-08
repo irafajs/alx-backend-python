ddbd509777552231bf388a8ad30362c2de046e93
irafajs <irafajs@gmail.com>
2024-04-08 10:50:31 +0200

    TASK03

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,11 +6,12 @@ Shebang to create a PY script
 
 import unittest
 from parameterized import parameterized
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, MagicMock
 
 
 access_nested_map = __import__('utils').access_nested_map
 get_json = __import__('utils').get_json
+memoize = __import__('utils').memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -54,3 +55,34 @@ class TestGetJson(unittest.TestCase):
         mock_get.assert_called_once_with(test_url)
 
         self.assertEqual(result, test_payload)
+
+
+class TestClass:
+    """class testclass"""
+    def a_method(self):
+        """method named a_method"""
+        return 42
+
+    @memoize
+    def a_property(self):
+        """method named a_property"""
+        return self.a_method()
+
+
+class TestMemoize(unittest.TestCase):
+    """class to test using Memoization"""
+
+    @patch.object(TestClass, 'a_method')
+    def test_memoize(self, mock_a_method):
+        """method to test method named memoize"""
+        mock_a_method.return_value = 42
+
+        test_instance = TestClass()
+
+        result1 = test_instance.a_property
+        result2 = test_instance.a_property
+
+        mock_a_method.assert_called_once()
+
+        self.assertEqual(result1, 42)
+        self.assertEqual(result2, 42)
