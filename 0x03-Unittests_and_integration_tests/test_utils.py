a3f47798b210258a4b9b7da13897cf19c02a9786
irafajs <irafajs@gmail.com>
2024-04-08 10:19:52 +0200

    TASK03

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,9 +6,11 @@ Shebang to create a PY script
 
 import unittest
 from parameterized import parameterized
+from unittest.mock import patch, Mock
 
 
 access_nested_map = __import__('utils').access_nested_map
+get_json = __import__('utils').get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -32,3 +34,23 @@ class TestAccessNestedMap(unittest.TestCase):
         """method to test exception raised"""
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """class to test get_json method from utils.py"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """method to test get_json using mock"""
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        result = get_json(test_url)
+
+        mock_get.assert_called_once_with(test_url)
+
+        self.assertEqual(result, test_payload)
